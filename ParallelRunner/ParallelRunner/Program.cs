300625ac107ebb3ecdc167db5a7b21b53cc6cfe5
unknown <csanchez@383VMX1.mustard.ipcoop.com>
2014-01-16 14:53:43 -0500

    read arguments

--- ParallelRunner/ParallelRunner/Program.cs
+++ ParallelRunner/ParallelRunner/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ParallelRunner
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var linesCount = ReadLastLinesCount(args);
+            var commandsToExecute = ReadExecutableFullPaths(args);
+        }
+
+        private static int ReadLastLinesCount(IEnumerable<string> args)
+        {
+            var lineCountArgStr = (args.FirstOrDefault(str => str.StartsWith("lc=")) ?? "lc=10");
+            var lineCountStr = lineCountArgStr.Split(new[] {"="},StringSplitOptions.RemoveEmptyEntries)[1];
+            return int.Parse(lineCountStr);
+        }
+
+        private static IEnumerable<string> ReadExecutableFullPaths(IEnumerable<string> lArgs)
+        {
+            foreach (var arg in lArgs)
+            {
+                if (File.Exists(arg))
+                {
+                    yield return arg;
+                }
+            }
         }
     }
 }
