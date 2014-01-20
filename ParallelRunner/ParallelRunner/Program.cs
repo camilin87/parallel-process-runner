94a1ce508d0d8b529b3f56c7a9a5c344928fb614
unknown <csanchez@383VMX1.mustard.ipcoop.com>
2014-01-20 08:36:27 -0500

    start processes and wait for completion

--- ParallelRunner/ParallelRunner/Program.cs
+++ ParallelRunner/ParallelRunner/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -10,9 +11,35 @@ namespace ParallelRunner
         private static void Main(string[] args)
         {
             var linesCount = ReadLastLinesCount(args);
-            var commandsToExecute = ReadExecutableFullPaths(args);
-        }
+            var commandsToExecute = ReadExecutableFullPaths(args).ToList();
 
+            var lProcess = new List<Process>();
+
+            Console.WriteLine("Starting {0} processes...", commandsToExecute.Count);
+
+            foreach (var fileName in commandsToExecute)
+            {
+                var process = new Process();
+                process.StartInfo.FileName = fileName;
+                process.StartInfo.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                lProcess.Add(process);
+            }
+
+            lProcess.ForEach(p => p.Start());
+
+            Console.WriteLine("Waiting for completion...");
+
+            foreach (var proc in lProcess)
+            {
+                proc.WaitForExit();
+                Console.WriteLine("Process \"{0}\" returned with exit code {1}", proc.StartInfo.FileName, proc.ExitCode);
+            }
+
+            //display the outputs
+
+            Console.ReadLine();
+        }
+        
         private static int ReadLastLinesCount(IEnumerable<string> args)
         {
             var lineCountArgStr = (args.FirstOrDefault(str => str.StartsWith("lc=")) ?? "lc=10");
